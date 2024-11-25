5ea1fb3e5442e4d367872cbcade4830450c51e9d
aucohol <aulmh.897@gmail.com>
2024-11-25 23:22:13 +0700

    BT 3

---
+++ BT 3/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontsize:{
+        h2:["48px","64px"],
+        lead1:["18px","32px"]
+      }
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
