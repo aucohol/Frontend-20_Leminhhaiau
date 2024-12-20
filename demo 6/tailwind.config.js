8a3da2d5aa0aa99fd1b934cd241343c1d88d858d
aucohol <aulmh.897@gmail.com>
2024-12-20 16:42:37 +0700

    BT 3

--- demo 6/tailwind.config.js
+++ demo 6/tailwind.config.js
@@ -1,19 +1,22 @@
 module.exports = {
   content: [
-    "./*.html",
-    
+    "./*.html"
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      fontSize:{
+      colors: {
+        primary:"#3ce1c6",
+        secondary:"#567AD7"
+      },
+      fontSize: {
+        h1:["72px","48px"],
         h2:["48px","64px"],
         lead1:["18px","32px"]
       },
-      spacing:{
-        93:["372"],
+      spacing: {
+        22: ["88px"]
       }
-      
     },
   },
   variants: {
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./*/*.html",
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary:"#3ce1c6",
+        secondary:"#D6B1FF",
+        addition: "8C30F5"
+      },
+      fontSize: {
+        h1:["72px","48px"],
+        h2:["48px","64px"],
+        h3:["40px","54px"],
+        h6:["20px","24px"],
+        lead1:["18px","32px"]
+      },
+      spacing: {
+        18: ["72px"],
+        22: ["88px"],
+        30: ["120px"]
+      },
+      fontFamily:{
+        roboto:"Roboto"
+      }
+    
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
